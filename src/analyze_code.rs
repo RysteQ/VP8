650fd7b8b5f2379cee394fbc8f666d01c694684b
RysteQ <640xdm460ds4@gmail.com>
2022-09-17 02:29:35 +0300

    Finished analyze_code.rs

--- src/analyze_code.rs
+++ src/analyze_code.rs
@@ -1,8 +1,9 @@
 use num_derive::{FromPrimitive, ToPrimitive};
 use parse_display::{Display, FromStr};
+use std::{u16, ptr::null};
 
 
-#[derive(Debug, Clone, Copy, FromPrimitive, ToPrimitive, Display, FromStr)]
+#[derive(Debug, Clone, Copy, FromPrimitive, ToPrimitive, Display, FromStr, PartialEq)]
 enum Opcode {
     ADC, AND, ASL, BCC, BCS, BEQ, BIT, BMI,
     BNE, BPL, BRK, BVC, BVS, CLC, CLD, CLI,
@@ -15,6 +16,7 @@ enum Opcode {
     LABEL,
 }
 
+#[derive(Debug)]
 enum AddressingMode {
     Immediate,
     ZeroPage,
@@ -35,25 +37,48 @@ pub struct Instruction {
 }
 
 pub fn get_instructions(instructions: Vec<String>) -> Vec<Instruction> {
-    let mut _to_: Vec<Instruction>;
+    let mut _to_return: Vec<Instruction> = Vec::new();
 
     for _i in 0..instructions.len() {
-        // TODO
+        let opcode_str: &str = &instructions[_i][0..3].trim();
+        let operand: &str = &instructions[_i][3..].trim();
+
+        let opcode: Opcode = get_opcode(opcode_str);
+
+        if opcode != Opcode::LABEL {
+            let addressing_mode: AddressingMode = get_addressing_mode(operand);
+            let value: u16 = get_operand_value(operand, addressing_mode);
+
+            _to_return.push(Instruction {
+                opcode: opcode,
+                addressing_mode: get_addressing_mode(operand),
+                value: value,
+                size: 1, // TODO: find a way to get the size of a given instruction
+                label_name: "".to_string()
+            });
+        } else {
+            _to_return.push(Instruction { 
+                opcode: opcode, 
+                addressing_mode: AddressingMode::Absolute, 
+                value: 0, 
+                size: 1, 
+                label_name: instructions.clone()[_i][0..&instructions.len() - 1].to_string()
+            });
+        }
     }
 
-    // return to_return
-    todo!();
+    _to_return
 }
 
-fn get_opcode(opcode_to_analyze: String) -> Opcode {
-    if !opcode_to_analyze.ends_with(':') {
+fn get_opcode(opcode_to_analyze: &str) -> Opcode {
+    if opcode_to_analyze.ends_with(':') == false {
         opcode_to_analyze.parse().unwrap()
     } else {
         Opcode::LABEL
     }
 }
 
-fn get_addressing_mode(parameters_to_analyze: String) -> AddressingMode {
+fn get_addressing_mode(parameters_to_analyze: &str) -> AddressingMode {
     match parameters_to_analyze.chars().next().unwrap() {
         '#' => {
             if parameters_to_analyze.len() == 4 {
@@ -103,6 +128,18 @@ fn get_addressing_mode(parameters_to_analyze: String) -> AddressingMode {
 
         _ => inform_error_and_exit("Error analyzing parameters", -1)
     }
+}
+
+fn get_operand_value(parameters: &str, addressing_mode: AddressingMode) -> u16 {
+    let mut hex_raw: String = String::new();
+
+    match addressing_mode {
+        AddressingMode::ZeroPage | AddressingMode::ZeroPageX | AddressingMode::IndirectX | AddressingMode::IndirectY => hex_raw = parameters[1..3].to_string(),
+        AddressingMode::Immediate => hex_raw = parameters[2..4].to_string(),
+        AddressingMode::Absolute | AddressingMode::AbsoluteX | AddressingMode::AbsoluteY => hex_raw = parameters[1..5].to_string()
+    }
+    
+    u16::from_str_radix(hex_raw.as_str(), 16).unwrap()
 }
 
 fn inform_error_and_exit(msg: &str, exit_code: i32) -> ! {
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ mod read_file;
 mod analyze_code;
 
 fn main() {
-    let arguments: Vec<String> = args().collect::<Vec<String>>();
+    let arguments: Vec<String> = vec!["lol".to_string(), "/home/rysteq/Desktop/Programs/Rust/VP8/test.asm".to_string()];//args().collect::<Vec<String>>();
 
-    if arguments.len() != 1 {
+    if arguments.len() != 2 {
         println!("Please input a path to the file");
         std::process::exit(-1);
     }
 
     let _file_data_lines: Vec<String> = read_file::read_file(arguments[1].to_string());
+    let _command_data = analyze_code::get_instructions(_file_data_lines);
 }
